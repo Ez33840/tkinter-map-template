b87e5d8c195d8389cae725a93ca2df58423c339b
ezequiel <zzzzz.cuenta001@gmail.com>
2023-08-01 17:28:58 -0300

    edite: ubicacion, fotos

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from controller.controlador_principal import ControladorPrincipal
 
 root = tk.Tk()
-root.title("Locales en la Zona")
+root.title("Recitales musicales ")
 controlador = ControladorPrincipal(root)
+
+
+
+
 root.mainloop()
