95bab46c28e37954937c84930011cf5e6ea7e438
ezequiel <zzzzz.cuenta001@gmail.com>
2023-08-01 20:21:17 -0300

    errores

--- app/controller/controlador_principal.py
+++ app/controller/controlador_principal.py
@@ -1,12 +1,12 @@
 from views.vista_principal import VistaPrincipal
-from models.local import Local
+from models.evento import Evento
 from models.ubicacion import Ubicacion
 from PIL import Image, ImageTk
 
 class ControladorPrincipal:
     def __init__(self, root):
         self.vista = VistaPrincipal(root, self.seleccionar_local, seleccionar_ubicacion)
-        self.locales = Local.cargar_locales("tkinter-map-template/app/data/locales.json")
+        self.locales = Evento.cargar_locales("tkinter-map-template/app/data/evento.json")
         self.ubicaciones = Ubicacion.cargar_ubicaciones("tkinter-map-template/app/data/ubicaciones.json")
         self.marcadores = []
         self.imagenes = []
@@ -27,7 +27,8 @@ class ControladorPrincipal:
     def cargar_marcadores(self):
         for ubicacion, local in zip(self.ubicaciones, self.locales):
             imagen = self.imagenes[ubicacion.id - 1]
-            marcador = self.vista.agregar_marcador_mapa(ubicacion.latitud, ubicacion.longitud, local.nombre, imagen)
+            latitud, longitud = ubicacion.coordenadas
+            marcador = self.vista.agregar_marcador_mapa(latitud, longitud, local.nombre, imagen)
             marcador.hide_image(True)
             self.marcadores.append(marcador)
 
@@ -46,7 +47,8 @@ class ControladorPrincipal:
                 break
         
         # Centra el mapa en la ubicación seleccionada
-        self.vista.mapa.set_position(ubicacion_seleccionada.latitud, ubicacion_seleccionada.longitud)
+        latitud, longitud = ubicacion_seleccionada.coordenadas
+        self.vista.mapa.set_position(latitud, longitud)
 
         print(f"Latitud: {ubicacion_seleccionada.latitud}, Longitud: {ubicacion_seleccionada.longitud}")
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from tkinter import*
 from controller.controlador_principal import ControladorPrincipal
 
+from tkintermapview import TkinterMapView
+
 root = Tk()
-root.title("Recitales musicales ")
+root.title("Recitales musicales")
 controlador = ControladorPrincipal(root)
+
+root.geometry("1100x700")
 
 
 
